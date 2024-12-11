5a77bdd4463525c72a868b2a954e0389ae4547f5
johnometalman <johnometalmanld@gmail.com>
2024-12-10 22:06:20 -0500

    manejo de respuesta de botones

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -3,9 +3,9 @@ import whatsappService from './whatsappService.js';
 class MessageHandler {
   async handleIncomingMessage(message, senderInfo) {
     if (message?.type === 'text') {
-      const incommingMessage = message.text.body.toLowerCase().trim();
+      const incomingMessage = message.text.body.toLowerCase().trim();
 
-      if(this.isGreeting(incommingMessage)){
+      if (this.isGreeting(incomingMessage)) {
         await this.sendWelcomeMessage(message.from, message.id, senderInfo);
         await this.sendWelcomeMenu(message.from); 
       } else {
@@ -13,6 +13,10 @@ class MessageHandler {
         await whatsappService.sendMessage(message.from, response, message.id);
       }
       
+      await whatsappService.markAsRead(message.id);
+    } else if (message?.type === 'interactive') {
+      const option = message?.interactive?.button_reply?.title.toLowerCase().trim();
+      await this.handleMenuOption(message.from, option);
       await whatsappService.markAsRead(message.id);
     }
   }
@@ -29,32 +33,51 @@ class MessageHandler {
   async sendWelcomeMessage(to, messageId, senderInfo) {
     const name = this.getSenderName(senderInfo);
     const firstName = name.split(' ')[0];
-    const welcomeMessage = `Hola ${firstName}, bienvenido/a a MEDPET, tu tienda de mascotas online, ¿En qué puedo ayudarte hoy? `;
+    const welcomeMessage = `Hola ${firstName}, bienvenido/a a MEDPET, tu tienda de mascotas online, ¿En qué puedo ayudarte hoy?`;
     await whatsappService.sendMessage(to, welcomeMessage, messageId);
   }
 
-
   async sendWelcomeMenu(to) {
-    const menuMessage =  "Elige una Opción"
+    const menuMessage = "Elige una Opción";
     const buttons = [
       {
-        type: 'reply', reply: {id: 'option_1', title: 'Agendar'}
+        type: 'reply', 
+        reply: { id: 'option_1', title: 'Agendar' }
       },
-
       {
-        type: 'reply', reply: {id: 'option_2', title: 'Consultar'}
+        type: 'reply', 
+        reply: { id: 'option_2', title: 'Consultar' }
       }, 
-
       {
-        type: 'reply', reply: {id: 'option_3', title: 'Ubicación'}
+        type: 'reply', 
+        reply: { id: 'option_3', title: 'Ubicación' }
       }
     ]; 
 
     await whatsappService.sendInteractiveButtons(to, menuMessage, buttons);
   }
 
+  async handleMenuOption(to, option) {
+    let response;
+    switch (option) {
+      case 'agendar':
+        response = 'Agendar Cita'; 
+        break;
+      
+      case 'consultar':
+        response = 'Consulta tu cita'; 
+        break;
 
+      case 'ubicación':
+        response = 'Esta es nuestra Ubicación'; 
+        break;
 
+      default:
+        response = 'Lo siento, no entendí tu selección, por favor selecciona una de las opciones';
+    }
+
+    await whatsappService.sendMessage(to, response);
+  }
 }
 
 export default new MessageHandler();
