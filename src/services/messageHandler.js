7e577a467badc8a8b03a0e95ee86d37f030505eb
johnometalman <johnometalmanld@gmail.com>
2024-12-27 12:05:52 -0500

    cambios del message handler y credentials de GCP

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -1,23 +1,50 @@
 import whatsappService from './whatsappService.js';
 
 class MessageHandler {
-  async handleIncomingMessage(message, senderInfo) {
-    if (message?.type === 'text') {
-      const incomingMessage = message.text.body.toLowerCase().trim();
+  constructor() {
+    this.appointmentState = {};
+  }
 
-      if (this.isGreeting(incomingMessage)) {
-        await this.sendWelcomeMessage(message.from, message.id, senderInfo);
-        await this.sendWelcomeMenu(message.from); 
-      } else {
-        const response = `Echo: ${message.text.body}`;
-        await whatsappService.sendMessage(message.from, response, message.id);
+  async handleIncomingMessage(message, senderInfo) {
+    try {
+      console.log('Incoming message:', message);
+      console.log('Sender info:', senderInfo);
+
+      const from = message.from; // Número del remitente
+
+      // Verificar si el usuario está en el flujo de agendamiento
+      if (this.appointmentState[from]) {
+        const incomingMessage = message?.text?.body?.trim();
+        await this.handleAppointmentFlow(from, incomingMessage);
+        return; // Detener aquí, no procesar más
       }
-      
-      await whatsappService.markAsRead(message.id);
-    } else if (message?.type === 'interactive') {
-      const option = message?.interactive?.button_reply?.title.toLowerCase().trim();
-      await this.handleMenuOption(message.from, option);
-      await whatsappService.markAsRead(message.id);
+
+      if (message?.type === 'text') {
+        const incomingMessage = message.text.body.toLowerCase().trim();
+        console.log('Incoming message text:', incomingMessage);
+
+        if (this.isGreeting(incomingMessage)) {
+          console.log('Greeting detected');
+          await this.sendWelcomeMessage(from, message.id, senderInfo);
+          await this.sendWelcomeMenu(from);
+        } else if (incomingMessage === 'media') {
+          console.log('Media request detected');
+          await this.sendMedia(from);
+        } else {
+          console.log('Echo response');
+          const response = `Echo: ${message.text.body}`;
+          await whatsappService.sendMessage(from, response, message.id);
+        }
+
+        await whatsappService.markAsRead(message.id);
+      } else if (message?.type === 'interactive') {
+        const option = message?.interactive?.button_reply?.title.toLowerCase().trim();
+        console.log('Interactive option:', option);
+        await this.handleMenuOption(from, option);
+        await whatsappService.markAsRead(message.id);
+      }
+    } catch (error) {
+      console.error('Error handling incoming message:', error);
     }
   }
 
@@ -41,18 +68,18 @@ class MessageHandler {
     const menuMessage = "Elige una Opción";
     const buttons = [
       {
-        type: 'reply', 
+        type: 'reply',
         reply: { id: 'option_1', title: 'Agendar' }
       },
       {
-        type: 'reply', 
+        type: 'reply',
         reply: { id: 'option_2', title: 'Consultar' }
-      }, 
+      },
       {
-        type: 'reply', 
+        type: 'reply',
         reply: { id: 'option_3', title: 'Ubicación' }
       }
-    ]; 
+    ];
 
     await whatsappService.sendInteractiveButtons(to, menuMessage, buttons);
   }
@@ -61,15 +88,16 @@ class MessageHandler {
     let response;
     switch (option) {
       case 'agendar':
-        response = 'Agendar Cita'; 
+        this.appointmentState[to] = { step: 'name' }; // Iniciar flujo de agendamiento
+        response = 'Por favor ingresa tu nombre:';
         break;
-      
+
       case 'consultar':
-        response = 'Consulta tu cita'; 
+        response = 'Consulta tu cita';
         break;
 
       case 'ubicación':
-        response = 'Esta es nuestra Ubicación'; 
+        response = 'Esta es nuestra Ubicación';
         break;
 
       default:
@@ -78,6 +106,77 @@ class MessageHandler {
 
     await whatsappService.sendMessage(to, response);
   }
+
+  async sendMedia(to) {
+    const mediaUrl = 'https://s3.amazonaws.com/gndx.dev/medpet-video.mp4';
+    const caption = '¡Esto es un video!';
+    const type = 'video';
+
+    await whatsappService.sendMediaMessage(to, type, mediaUrl, caption);
+  }
+
+  completeAppointment(to) {
+    const appointment = this.appointmentState[to];
+    delete this.appointmentState[to];
+
+    const userData = [
+      to, 
+      appointment.name, 
+      appointment.petName,
+      appointment.petType, 
+      appointment.reason,
+      new Date().toISOString()
+    ]
+
+    console.log(userData);
+
+  return `Gracias por agendar tu cida. 
+  *Reumen de tu Cita:*
+  
+  *Nombre:* ${appointment.name}
+  *Nombre de la Mascota:* ${appointment.petName}
+  *Tipo de Mascota:* ${appointment.petType}
+  *Razón de Consulta:* ${appointment.reason}
+  
+  Nos pondremos en contacto contigo pronto para confirmar la fecha y hora de tu cita`
+  }
+
+  async handleAppointmentFlow(to, message) {
+    const state = this.appointmentState[to];
+    let response;
+
+    switch (state.step) {
+      case 'name':
+        state.name = message;
+        state.step = 'petName';
+        response = "Gracias, ahora, ¡Cuál es el nombre de tu mascota?";
+        break;
+
+      case 'petName':
+        state.petName = message;
+        state.step = 'petType';
+        response = '¿Qué tipo de mascota es? (por ejemplo: perro, gato, huron, etc.)';
+        break;
+
+      case 'petType':
+        state.petType = message;
+        state.step = 'reason';
+        response = '¿Cuál es el motivo de tu consulta?';
+        break;
+
+      case 'reason':
+        state.reason = message;
+        response = this.completeAppointment(to);
+        delete this.appointmentState[to]; 
+        break;
+
+      default:
+        response = 'Lo siento, no entendí tu mensaje. Por favor, responde a la pregunta anterior.';
+    }
+    await whatsappService.sendMessage(to, response);
+  }
+
+  
 }
 
 export default new MessageHandler();
