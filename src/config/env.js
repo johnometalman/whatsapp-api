1df0d61d6093885a06e7e7218b1d70bbd8898003
johnometalman <johnometalmanld@gmail.com>
2024-12-28 10:12:03 -0500

    Uso de template literals para la API y el Prompt

--- src/config/env.js
+++ src/config/env.js
@@ -9,5 +9,6 @@ export default {
   BUSINESS_PHONE: process.env.BUSINESS_PHONE,
   API_VERSION: process.env.API_VERSION,
   PORT: process.env.PORT || 3000,
-  CHATPGT_API_KEY: process.env.CHATPGT_API_KEY
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  PROMPT: process.env.PROMPT
 };
--- src/services/openAIService.js
+++ src/services/openAIService.js
@@ -1,24 +1,32 @@
 import OpenAI from "openai";
 import config from "../config/env.js";
 
-const client = new OpenAI ({
-    apiKey: config.CHATPGT_API_KEY,
-}); 
+const client = new OpenAI({
+    apiKey: config.OPENAI_API_KEY,
+});
 
 const openAIService = async (message) => {
     try {
         const response = await client.chat.completions.create({
-            messages: [{ role: 'system', content: 'Comportate como un veterinario. Responde de forma sencilla y amigable, como si fuera una conversación por WhatsApp. Responde de manera cálida y profesional, con 3 a 4 frases por respuesta, evitando generar conversaciones adicionales. Si la situación lo amerita, sugiere que el dueño agende una consulta en persona. Si es necesario, ofrece información o consejos relacionados con la salud del animal, pero no sugieras productos. Utiliza un lenguaje cercano, como si estuvieras hablando con el dueño de su perrito o gatito. Si el dueño quiere hacer una cita, ofrece la posibilidad de agendar directamente la consulta por la misma conversación.'}, 
-                        {role: 'user', content: message}],
-
+            messages: [
+                {
+                    role: 'system',
+                    content: `${config.PROMPT}`
+                },
+                { role: 'user', content: message }
+            ],
             model: 'gpt-4o'
         });
 
-        return response.choices[0].messages.content;
+        // Log the response to verify its structure
+        console.log(response);
 
-    } catch (error){
+        // Correctly access the 'content' property
+        return response.choices[0].message.content;
+
+    } catch (error) {
         console.error(error);
     }
 }
 
-export default openAIService; 
+export default openAIService;
